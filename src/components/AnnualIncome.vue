b6acb5be49fd3d61f21a4ebbcd09fcc2456d16dc
Mollie Goforth <molliegoforth818@gmail.com>
2021-03-15 09:42:39 -0500

    saving changes

---
+++ src/components/AnnualIncome.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <v-form>
+          <v-text-field outlined label="annualIncome" v-model="">
+      </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+props: ["annualAmount"]
+}
+</script>
+
+<style>
+
+</style>
