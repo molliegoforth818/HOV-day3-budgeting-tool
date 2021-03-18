4c162b74a4b255d1d8f8a34c7849a778ea14265c
Mollie Goforth <molliegoforth818@gmail.com>
2021-03-18 18:00:23 -0500

    monthly net and annual net colors working

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <v-app>
-    <v-app-bar app color="pink lighten-3" dark>
-<h4>Monthly Budget</h4>
-    </v-app-bar>
     <annual-income :annualAmount="annualIncome"/>
     <expenses
     :expenses="monthlyExpenses"
     :total-amount="annualExpenses"
     />
+    <income-card title="Monthly Net" :net-amount=-500 />
+    <income-card title="Annual Net" :net-amount=500 />
   </v-app>
 </template>
 
 <script>
 import AnnualIncome from './components/AnnualIncome';
 import Expenses from './components/Expenses';
+import IncomeCard from './components/IncomeCard'
 export default {
-components: { AnnualIncome, Expenses, },
+components: { AnnualIncome, Expenses, IncomeCard, },
   data() {
     return {
     annualIncome: 0,
---
+++ src/components/IncomeCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card>
+         <v-card-title class="pink--text">{{title}}</v-card-title>
+    <v-card-text>
+<h2 class="net-amount" :class="incomeColor">{{displayAmount}}</h2>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+props:["title", "netAmount"],
+computed: {
+    displayAmount() {
+        return new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(this.netAmount);
+    },
+    incomeColor() {
+       return {
+           "green--text": this.netAmount>=0,
+           "red--text": this.netAmount<0
+       } 
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
