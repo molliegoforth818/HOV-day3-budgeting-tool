7df43ee9202f8e9fd127eb73122efb909857af53
Mollie Goforth <molliegoforth818@gmail.com>
2021-03-18 18:24:09 -0500

    computed properties added to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,42 @@
 <template>
   <v-app>
-    <annual-income :annualAmount="annualIncome"/>
-    <expenses
-    :expenses="monthlyExpenses"
-    :total-amount="annualExpenses"
-    />
-    <income-card title="Monthly Net" :net-amount=-500 />
-    <income-card title="Annual Net" :net-amount=500 />
+    <annual-income :annualAmount="annualIncome" />
+    <expenses :expenses="monthlyExpenses" :total-amount="annualExpenses" />
+    <income-card title="Monthly Net" :net-amount="monthlyNet" />
+    <income-card title="Annual Net" :net-amount="annualNet" />
   </v-app>
 </template>
 
 <script>
-import AnnualIncome from './components/AnnualIncome';
-import Expenses from './components/Expenses';
-import IncomeCard from './components/IncomeCard'
+import AnnualIncome from "./components/AnnualIncome";
+import Expenses from "./components/Expenses";
+import IncomeCard from "./components/IncomeCard";
 export default {
-components: { AnnualIncome, Expenses, IncomeCard, },
+  components: { AnnualIncome, Expenses, IncomeCard },
   data() {
     return {
-    annualIncome: 0,
-    monthlyExpenses: [],  
+      annualIncome: 0,
+      monthlyExpenses: [],
+    };
+  },
+  computed: {
+    monthlyIncome() {
+      return this.annualIncome / 12;
+    },
+    totalMonthlyExpenses() {
+      return this.monthlyExpenses.reduce((total, month) => {
+        return total + month.amount;
+      }, 0);
+    },
+    annualExpenses() {
+      return this.monthlyExpenses * 12;
+    },
+    monthlyNet() {
+      return (this.monthlyIncome - this.totalMonthlyExpenses)
+    },
+    annualNet() {
+      return +(this.annualIncome - this.annualExpenses)
     }
-  }
+  },
 };
 </script>
